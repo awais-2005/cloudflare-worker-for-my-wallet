503257215e40cadafbad61b1670066d8c9c8310d
awais-2005 <rmawais852689@gmail.com>
2026-02-02 17:21:33 +0500

    return id on login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -24,7 +24,7 @@ export async function login(c) {
     throw new ApiError(401, 'Invalid credentials');
   }
   const token = await signJwt({ id: user.id, email: user.email }, c.env);
-  return c.json({ token });
+  return c.json({ id: user.id, token });
 }
 
 export async function sendOTP(c) {
